51ab57d1665b2f8d1882d24c2b4cf9b1f6b1faa2
adniq5 <adniq55@gmail.com>
2024-12-27 21:50:56 +0800

    U:Menambahkan Fitur Tambah Data

--- CRUDT4.cpp
+++ CRUDT4.cpp
@@ -1,63 +1,80 @@
 #include <iostream>
-#include <conio.h>
+#include <cstdlib>  // Untuk system("cls")
+#include <conio.h>  // Untuk getch()
+#include <string>
 using namespace std;
 
+// Struktur Data Mahasiswa
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string tanggalLahir; // Format DD/MM/YYYY
+    float ipk;
+};
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
+// Data mahasiswa
+Mahasiswa dataMahasiswa[30];
+int pos = -1; // Menyimpan posisi terakhir data mahasiswa
 
+void tampilkanMenu() {
+    system("cls");
+    cout << "\n=== Menu Aplikasi Data Mahasiswa ===\n";
+    cout << "1. Masukkan Data Mahasiswa\n";
+    cout << "2. Tampilkan Data Mahasiswa\n";
+    cout << "3. Update Data Mahasiswa\n";
+    cout << "4. Hapus Data Mahasiswa\n";
+    cout << "5. Exit\n";
+    cout << "Masukkan pilihan: ";
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
+void tambahData() {
+    system("cls");
+    if (pos >= 29) {
+        cout << "\nData mahasiswa penuh! Tidak bisa menambah data baru.\n";
+        return;
+    }
 
+    pos++;
+    cout << "\nMasukkan Data Mahasiswa\n";
+    cout << "Masukkan NIM: ";
+    getline(cin, dataMahasiswa[pos].nim);
+    cout << "Masukkan Nama: ";
+    getline(cin, dataMahasiswa[pos].nama);
+    cout << "Masukkan Alamat: ";
+    getline(cin, dataMahasiswa[pos].alamat);
+    cout << "Masukkan Tanggal Lahir (DD/MM/YYYY): ";
+    getline(cin, dataMahasiswa[pos].tanggalLahir);
+    cout << "Masukkan IPK: ";
+    cin >> dataMahasiswa[pos].ipk;
+    cin.ignore(); // Menghindari masalah dengan buffer input
+}
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    char pilihan;
+    do {
+        tampilkanMenu();
+        pilihan = getch();
+        switch (pilihan) {
+            case '1':
+                tambahData();
+                break;
+            case '2':
+                // Tampilkan data
+                break;
+            case '3':
+                // Update data
+                break;
+            case '4':
+                // Hapus data
+                break;
+            case '5':
+                cout << "\nTerima kasih telah menggunakan program ini.\n";
+                break;
+            default:
+                cout << "\nPilihan tidak valid. Coba lagi.\n";
+                break;
+        }
+    } while (pilihan != '5');
+    return 0;
 }
